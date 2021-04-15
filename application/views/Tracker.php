c9dfe4d310100b35ffeb41e61004a93191febd68
Bryan Sampiano <bryansampi08@gmail.com>
2021-04-15 08:25:31 +0800

    Remove:Browser Tab Icon

--- application/views/Tracker.php
+++ application/views/Tracker.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>assets/img/apple-icon.png" />
-	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/img/favicon.png" />
+	<!-- <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>assets/img/apple-icon.png" />
+	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/img/favicon.png" /> -->
 	<title>Student Enrollment Tracker</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
